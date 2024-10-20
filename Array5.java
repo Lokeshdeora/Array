45385f97eba91a0e9af126f34864061c9603759b
Lokesh Deora <lokeshdeorajodhpur@gmail.com>
2024-10-20 18:23:21 +0530

    Add files via upload

---
+++ Array5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+     public class Array5 {
+     
+          public static void main(String[] args){
+              Scanner sc=new Scanner(System.in);
+              System.out.println("enter size of array");
+              int n=sc.nextInt();
+              int a[] =new int[n];
+              for(int i=0;i<n;i++){
+              }}
+     }
